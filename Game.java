bcc14060a7c737a060cb17cc386ef70dbebac55e
Ian Dai <ianbbdai@gmail.com>
2022-05-20 23:30:10 -0700

    basically initial commit of window

---
+++ Game.java
@@ -0,0 +1,16 @@
+import java.awt.Graphics2D;
+/*
+simply make a rectangle that moves by wasd or arrow keys
+Add some way to shoot projectiles with the mouse and a key press
+
+Then use the sockets to make a multiplayer game. 
+*/
+public class Game {
+    public Game() {
+
+    }
+
+    public void draw(Graphics2D g) {
+
+    }
+}
